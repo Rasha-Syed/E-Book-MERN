c99fcd5d521f8f0e805b5b686e16516141a629e3
Rasha-Syed <rashanishath@gmail.com>
2025-03-16 07:42:26 +0530

    Update book.js

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -4,54 +4,7 @@ const jwt=require("jsonwebtoken");
 const Book=require("../models/book");
 const {authenticateToken}=require("./userAuth");
 
-//add book -admin
-/*router.post("/add-book",authenticateToken,async(req,res)=>{
-    try{
-        const {id}=req.headers;
-        const user=await User.findById(id);
-        if(user.role!=="admin")
-        {
-            return res.status(500).json({message: "You are not having access to perform admin tasks"});
-        }
-        const book=new Book({
-            url:req.body.url,
-            title:req.body.title,
-            author:req.body.author,
-            price:req.body.price,
-            desc:req.body.desc,
-            language:req.body.language,
-        });
-        await book.save();
-        res.status(200).json({message:"Book created successfully"});
-    }catch(error)
-    {
-        res.status(500).json({message: "Internal server error"});
-    }
-});
 
-
-
-//update book
-router.put("/update-book",authenticateToken,async(req,res)=>{
-    try{
-        const {bookid}=req.headers;
-        
-        await Book.findByIdAndUpdate(bookid,{
-            url:req.body.url,
-            title:req.body.title,
-            author:req.body.author,
-            price:req.body.price,
-            desc:req.body.desc,
-            language:req.body.language,
-        });
-        
-        return res.status(200).json({message:"Book updated successfully"});
-    }catch(error)
-    {
-        //console.log(error);
-        return res.status(500).json({message: "An error occured"});
-    }
-});*/
 // add-book route
 router.post("/add-book", authenticateToken, async (req, res) => {
     try {
